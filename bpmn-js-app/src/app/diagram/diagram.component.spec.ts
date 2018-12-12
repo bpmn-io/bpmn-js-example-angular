0d7fd1fa905868992e2d4dc4cd9ee6a1f96d7796
Nico Rehwaldt <git_nikku@nixis.de>
2018-12-12 15:56:15 +0100

    chore(example): improve
    * make diagram importing a reactive thing
    * improve import error styling
    * drop `changeDetection` config (users are able to customize their apps,
    if they know what they are doing)
    * add bpmn-js distribution hint
    * rename import event
    * remove state from DiagramComponent (not needed),
      testing emitted events instead
    * removing retries logic from DiagramComponent; users
      may add this stuff back, if they know what they are
      doing.

--- bpmn-js-app/src/app/diagram/diagram.component.spec.ts
+++ bpmn-js-app/src/app/diagram/diagram.component.spec.ts
@@ -4,17 +4,52 @@ import {
   getTestBed,
   TestBed
 } from '@angular/core/testing';
+
 import { DebugNode } from '@angular/core';
+
 import {
   HttpClientTestingModule,
   HttpTestingController
 } from '@angular/common/http/testing';
+
 import { DiagramComponent } from './diagram.component';
 
-const diagramUrl =
-  'https://cdn.rawgit.com/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
+
+const BPMN_DIAGRAM = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL"
+               xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI"
+               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+               id="sid-38422fae-e03e-43a3-bef4-bd33b32041b2"
+               targetNamespace="http://bpmn.io/bpmn"
+               exporter="http://bpmn.io"
+               exporterVersion="0.10.1">
+    <process id="Process_1" isExecutable="false" />
+    <bpmndi:BPMNDiagram id="BpmnDiagram_1">
+      <bpmndi:BPMNPlane id="BpmnPlane_1" bpmnElement="Process_1" />
+    </bpmndi:BPMNDiagram>
+  </definitions>
+`;
+
+const BPMN_DIAGRAM_WITH_WARNINGS = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL"
+               xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI"
+               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+               id="sid-38422fae-e03e-43a3-bef4-bd33b32041b2"
+               targetNamespace="http://bpmn.io/bpmn"
+               exporter="http://bpmn.io"
+               exporterVersion="0.10.1">
+    <process id="Process_1" isExecutable="false" a:b="C" />
+    <bpmndi:BPMNDiagram id="BpmnDiagram_1">
+      <bpmndi:BPMNPlane id="BpmnPlane_1" bpmnElement="Process_1" />
+    </bpmndi:BPMNDiagram>
+  </definitions>
+`;
+
 
 describe('DiagramComponent', () => {
+
   let httpMock: HttpTestingController;
   let fixture: ComponentFixture<DiagramComponent>;
   let component: DebugNode['componentInstance'];
@@ -26,37 +61,96 @@ describe('DiagramComponent', () => {
       imports: [HttpClientTestingModule],
       declarations: [DiagramComponent]
     });
+
     fixture = TestBed.createComponent(DiagramComponent);
     component = fixture.debugElement.componentInstance;
     httpMock = injector.get(HttpTestingController);
     fixture.detectChanges();
   }));
 
-  afterEach(() => httpMock.verify());
+  afterEach(() => {
+    httpMock.verify();
 
-  it('should create and tear down', () => {
-    expect(component).toBeTruthy();
     fixture.destroy();
   });
 
-  it('makes a successful get request and clears any existing error messages', () => {
-    component.url = diagramUrl;
-    spyOn(component, 'loadUrl').and.callThrough();
-    component.loadUrl(diagramUrl);
 
-    httpMock.expectOne({url: diagramUrl, method: 'GET'});
-    expect(component.errorMessage).toEqual('');
+  it('should create and tear down', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('makes an unsuccessful get request and sets an error message', () => {
-    component.url = null;
-    spyOn(component, 'loadUrl').and.callThrough();
-    spyOn(console, 'error');
-    component.loadUrl();
 
-    httpMock.expectNone(diagramUrl);
-    expect(component.errorMessage).toEqual(
-      'ERROR: diagram did not load - please check the console.'
-    );
+  it('should load and render successfully', (done) => {
+
+    // given
+    const diagramURL = 'some-url';
+
+    component.importDone.subscribe(result => {
+      // then
+      expect(result).toEqual({
+        type: 'success',
+        warnings: []
+      });
+
+      done();
+    });
+
+    // when
+    component.loadUrl(diagramURL);
+
+    const request = httpMock.expectOne({ url: diagramURL, method: 'GET' });
+
+    request.flush(BPMN_DIAGRAM);
   });
+
+
+  it('should expose import warnings', (done) => {
+
+    // given
+    const diagramURL = 'some-url';
+
+    component.importDone.subscribe(result => {
+      // then
+      expect(result.type).toEqual('success');
+
+      expect(result.warnings.length).toEqual(1);
+      expect(result.warnings[0].message).toContain('unparsable content <process> detected');
+
+      done();
+    });
+
+    // when
+    component.loadUrl(diagramURL);
+
+    const request = httpMock.expectOne({ url: diagramURL, method: 'GET' });
+
+    request.flush(BPMN_DIAGRAM_WITH_WARNINGS);
+  });
+
+
+  it('should fail to load and render', (done) => {
+
+    // given
+    const diagramURL = 'some-url';
+
+    // when
+    component.loadUrl(diagramURL);
+
+    component.importDone.subscribe(result => {
+
+      // then
+      expect(result.type).toEqual('error');
+      expect(result.error.message).toEqual('Http failure response for some-url: 404 FOO');
+
+      done();
+    });
+
+    const request = httpMock.expectOne({ url: diagramURL, method: 'GET' });
+
+    request.flush('Not Found', {
+      status: 404,
+      statusText: 'FOO'
+    });
+  });
+
 });
---
+++ bpmn-js-app/src/app/diagram/rx.ts
@@ -0,0 +1,38 @@
+import { Observable } from 'rxjs';
+
+/**
+ * An operator that imports the first XML piped via the piped diagram XML
+ * into the passed BpmnJS instance.
+ */
+export const importDiagram = (bpmnJS) => <Object>(source: Observable<string>) =>
+  new Observable<string>(observer => {
+
+    const subscription = source.subscribe({
+      next(xml: string) {
+
+        console.log('IMPORT DIAGRAM');
+
+        // canceling the subscription as we are interested
+        // in the first diagram to display only
+        subscription.unsubscribe();
+
+        bpmnJS.importXML(xml, function(err, warnings) {
+
+          if (err) {
+            observer.error(err);
+          } else {
+            observer.next(warnings);
+          }
+
+          observer.complete();
+        });
+      },
+      error(e) {
+        console.log('ERROR');
+        observer.error(e);
+      },
+      complete() {
+        observer.complete();
+      }
+    });
+  });
