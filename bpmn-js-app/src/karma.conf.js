8ac230d4d9d29ab95f4474997a6b8832e5eda43d
Kosmas Schütz <kosmas.schuetz@gmx.com>
2020-04-26 15:43:18 +0200

    test(karma): trying to get it run on Travis CI

--- bpmn-js-app/src/karma.conf.js
+++ bpmn-js-app/src/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu' ]
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-translate', '--disable-extensions']
       }
     },
     reporters: ['progress', 'kjhtml'],
